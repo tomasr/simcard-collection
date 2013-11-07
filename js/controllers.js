fa6bc299a88c3266c6879bc2562f6bde87290895
Tomas Restrepo <tomas@winterdom.com>
2013-11-07 09:56:37 -0500

    Add actual images for some cards, add blank image and add popup

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,38 @@
 var app = angular.module('SCC', ['ngGrid']);
 
-app.controller('SCController', function($scope,$http) {
+app.filter('groupBy', function() {
+  return function(items, groupedBy) {
+    if (items) {
+      var finalItems = [], thisGroup;
+      for (var i = 0; i < items.length; i++) {
+        if (!thisGroup) {
+          thisGroup = [];
+        }
+        thisGroup.push(items[i]);
+        if (((i+1) % groupedBy) == 0) {
+          finalItems.push(thisGroup);
+          thisGroup = null;
+        }
+      };
+      if (thisGroup) {
+        finalItems.push(thisGroup);
+      }
+      return finalItems;
+    }
+  };
+});
+
+app.controller('SCController', function($scope,$http,$filter) {
   $scope.sims = [];
+  $scope.simsGroupedBy2 = [];
+  $scope.currentImg = 'images/blank-full.png';
+  $scope.imageTitle = '';
+
   $scope.loadData = function() {
     var httpRequest = $http.get('mint.json').success(
       function(data) {
-        console.log('completed request: ' + data);
         $scope.sims = angular.fromJson(data).result;
+        $scope.simsGroupedBy2 = $filter('groupBy')($scope.sims, 2);
         if (!$scope.$$phase) {
           $scope.$apply();
         }
@@ -26,5 +52,15 @@ app.controller('SCController', function($scope,$http) {
       {field: 'description', width: "*", displayName: 'Description'},
     ]
   };
+  $scope.showImage = function(sim, image) {
+    if ( image === 'front' ) {
+      $scope.imageTitle = sim.serial + ' (Front)';
+      $scope.currentImg = "images/full/" + sim.imgfront;
+    } else {
+      $scope.imageTitle = sim.serial + ' (Back)';
+      $scope.currentImg = "images/full/" + sim.imgback;
+    }
+    $('#myModal').modal({show: true});
+  }
   $scope.loadData();
 });
