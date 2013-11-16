2caaf66930dd4041f1e5c297e775c7dad15fa1e4
Tomas Restrepo <tomas@winterdom.com>
2013-11-15 22:34:30 -0500

    Filters working with URL parameters

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,17 @@ app.config(['$routeProvider', function($routeProvider, $locationProvider) {
       controller: DetailController,
       controllerAs: 'detail'
     });
-    $routeProvider.when('', {
+    $routeProvider.when('/country/:country', {
+      templateUrl: 'all.html',
+      controller: AllController,
+      controllerAs: 'all'
+    });
+    $routeProvider.when('/country/:country/network/:network', {
+      templateUrl: 'all.html',
+      controller: AllController,
+      controllerAs: 'all'
+    });
+    $routeProvider.otherwise({
       templateUrl: 'all.html',
       controller: AllController,
       controllerAs: 'all'
@@ -15,6 +25,17 @@ app.config(['$routeProvider', function($routeProvider, $locationProvider) {
     //$locationProvider.html5Mode(true);
 }]);
 
+app.directive('addOptionAll',
+  function($timeout) {
+    return {
+      link : function(scope, element, attrs) {
+        // use $timeout if you want to add "All" at the bottom else dont use                
+        $timeout(function(){
+          element.prepend("<option value='*'> All</option>");
+        }, 1000);
+     }
+  };
+});
 app.filter('groupBy', function() {
   return function(items, groupedBy) {
     if (items) {
@@ -35,6 +56,21 @@ app.filter('groupBy', function() {
       return finalItems;
     }
   };
+});
+
+app.filter('unique-fvalue', function() {
+  return function(items, property) {
+    if ( items ) {
+      var nitems = [];
+      for ( var i=0; i < items.length; i++ ) {
+        if ( nitems.indexOf(items[i][property]) < 0 ) {
+          nitems.push(items[i][property]);
+        }
+      }
+      nitems.sort();
+      return nitems;
+    }
+  }
 });
 
 app.directive('ngHolder', function() {
@@ -66,17 +102,18 @@ function groupByField(list, getter) {
 }
 
 
-function SCController($scope,$http,$filter) {
+function SCController($scope, $http, $filter) {
   $scope.sims = [];
-  $scope.networkList = [];
-  $scope.countryList = [];
+  $scope.networkList = null;
+  $scope.countryList = null;
 
   $scope.loadData = function() {
     var httpRequest = $http.get('mint.json').success(
       function(data) {
-        $scope.sims = angular.fromJson(data).result;
-        $scope.networkList = groupByField($scope.sims, function (obj) { return obj.mobileNetwork; });
-        $scope.countryList = groupByField($scope.sims, function (obj) { return obj.country; });
+        var json = angular.fromJson(data);
+        $scope.sims = json.sims;
+        $scope.countryList = json.countries;
+        $scope.networkList = $filter('unique-fvalue')($scope.sims, 'mobileNetwork');
         if (!$scope.$$phase) {
           $scope.$apply();
         }
@@ -85,21 +122,36 @@ function SCController($scope,$http,$filter) {
 
   $scope.findSimBySerial = function(serial) {
     for ( i=0; i < $scope.sims.length; i++ ) {
-      if ( $scope.sims[i].serial == serial ) {
+      if ( $scope.sims[i].serial === serial ) {
         return $scope.sims[i];
       }
     }
     return null;
   };
 
+  $scope.simsByCountry = function(country) {
+    if ( country === '*' ) {
+      return $scope.sims;
+    }
+    var result = [];
+    for ( i=0; i < $scope.sims.length; i++ ) {
+      if ( $scope.sims[i].country === country ) {
+        result.push($scope.sims[i]);
+      }
+    }
+    return result;
+  }
+
   $scope.loadData();
 }
 
-function AllController($scope, $filter) {
+function AllController($scope, $filter, $location, $routeParams) {
   $scope.matchingSimsGrouped = [];
   $scope.matchingSims = [];
   $scope.currentImg = 'images/blank-full.png';
   $scope.imageTitle = '';
+  $scope.selectedCountry = $routeParams.country;
+  $scope.selectedNetwork = $routeParams.network;
 
   $scope.showImage = function(sim, image) {
     if ( image === 'front' ) {
@@ -118,9 +170,19 @@ function AllController($scope, $filter) {
     $scope.matchingSimsGrouped = $filter('groupBy')($scope.matchingSims, 2);
   }
 
+  $scope.navigateToCountry = function() {
+    $location.path('/country/' + $scope.selectedCountry);
+  }
+  $scope.navigateToNetwork = function() {
+    if ( !$scope.selectedCountry ) {
+      $scope.selectedCountry = '*';
+    }
+    $location.path('/country/' + $scope.selectedCountry + '/network/' + $scope.selectedNetwork);
+  }
+
   $scope.getMatchingCards = function() {
     var result = [];
-    var cardsByCountry = $scope.$parent.countryList[$scope.selectedCountry];
+    var cardsByCountry = $scope.$parent.simsByCountry($scope.selectedCountry);
     if ( !cardsByCountry ) {
       return result;
     }
@@ -141,6 +203,9 @@ function AllController($scope, $filter) {
     $scope.currentImg = "images/blank-full.png";
     $scope.$apply();
   });
+  if ( $scope.selectedCountry ) {
+    $scope.filterData();
+  }
 }
 
 
